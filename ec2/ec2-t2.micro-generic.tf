7b69faa7d917a711f0ec6e81da84bce32689d5c1
Stephen Morgan <smorgan@privoit.com>
2021-04-08 09:39:02 -0700

    updated ec2-t2.micro-generic.tf

--- ec2/ec2-t2.micro-generic.tf
+++ ec2/ec2-t2.micro-generic.tf
@@ -2,12 +2,6 @@ resource "aws_instance" "SMorganEC2Generic" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
 
-  # the VPC subnet
-  subnet_id = aws_subnet.main-public-1.id
-
-  # the security group
-  vpc_security_group_ids = [aws_security_group.example-instance.id]
-
   # the public SSH key
-  key_name = aws_key_pair.mykeypair.key_name
+  key_name = aws_key_pair.mykey.key_name
 }
