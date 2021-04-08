8e226cdfdafa5b9957f90c156294b1ce6dcf1c7e
Stephen Morgan <smorgan@privoit.com>
2021-04-08 09:45:49 -0700

    updated ec2

--- ec2/ec2-t2.micro-generic.tf
+++ ec2/ec2-t2.micro-generic.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "SMorganEC2Generic" {
-  ami           = var.AMIS[var.AWS_REGION]
+  ami           = "ami-0533f2ba8a1995cf9"
   instance_type = "t2.micro"
+  
+
+  tags = {
+    Name        = "steves_ec2"
+    Environment = "Sandbox"
+  }
 
   # the public SSH key
   key_name = aws_key_pair.mykey.key_name
